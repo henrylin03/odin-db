4612d96a2d866efce0a86ebc582a662629fd9afc
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-10-28 22:42:24 +1100

    feat: use EJS instead of HTML. use PicoCSS for quick and consistent styling for HTML and vanilla JS. add display of all the usernames on the home page, and refactor form to add username from a raw html file to a EJS view file

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const indexRouter = require("./routes/indexRouter");
 const app = express();
 const basePath = path.join(__dirname, "public");
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(basePath));
