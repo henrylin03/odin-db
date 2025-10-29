8eb55e08d259e104d22f4f7aa843be1dc333a03d
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-10-30 08:40:10 +1100

    feat(index.ejs): display button to take users back to all users from their searched usernames (essentially 'unfiltering')
    decided it is best to have a separate boolean value in what is being passed from indexController to the view, rather than just checking in the view whether the title has the word 'Search' or 'results' in it for example. I think this is more readable IMO.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,15 +2,19 @@ const db = require("../db/queries");
 
 async function getUsernames(req, res) {
   const searchbarValue = req.query.search;
+  let isFiltered = false;
 
   if (searchbarValue === "" || searchbarValue === undefined) {
     const usernames = await db.getAllUsernames();
-    res.render("index", { title: "Users", usernames });
+    res.render("index", { title: "Users", usernames, isFiltered });
   } else {
     const searchedUsernames = await db.getUsernames(searchbarValue);
+    isFiltered = true;
+
     res.render("index", {
       title: "Search results",
       usernames: searchedUsernames,
+      isFiltered,
     });
   }
 }
