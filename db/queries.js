f390eedcbee80cbfdcf86d0dcbe183356c5f41af
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-10-29 21:52:18 +1100

    feat(queries): add function to get username based on searchText

--- db/queries.js
+++ db/queries.js
@@ -5,6 +5,15 @@ async function getAllUsernames() {
   return rows;
 }
 
+async function getUsernames(searchText) {
+  const { rows } = await pool.query(
+    "SELECT * FROM usernames WHERE username LIKE %$1%",
+    [searchText],
+  );
+  console.log("filtered users:", rows);
+  return rows;
+}
+
 async function insertUsername(username) {
   // `$1` inserts username into query indirectly, through pg's query-parametisation. this prevents sql injections.
   await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
@@ -12,5 +21,6 @@ async function insertUsername(username) {
 
 module.exports = {
   getAllUsernames,
+  getUsernames,
   insertUsername,
 };
