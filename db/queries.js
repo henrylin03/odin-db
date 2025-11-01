7c069c798c03e99ca74b392be0fdc2cfa33c232c
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-11-01 14:07:39 +1100

    feat: enable users to delete all usernames from the database
    used a db query that is 'DELETE FROM tableName'

--- db/queries.js
+++ db/queries.js
@@ -1,25 +1,30 @@
 const pool = require("./pool");
 
 async function getAllUsernames() {
-  const { rows } = await pool.query("SELECT * FROM usernames");
-  return rows;
+	const { rows } = await pool.query("SELECT * FROM usernames");
+	return rows;
 }
 
 async function getUsernames(searchText) {
-  const { rows } = await pool.query(
-    "SELECT * FROM usernames WHERE LOWER(username) LIKE LOWER($1)",
-    [`%${searchText}%`],
-  );
-  return rows;
+	const { rows } = await pool.query(
+		"SELECT * FROM usernames WHERE LOWER(username) LIKE LOWER($1)",
+		[`%${searchText}%`],
+	);
+	return rows;
 }
 
 async function insertUsername(username) {
-  // `$1` inserts username into query indirectly, through pg's query-parametisation. this prevents sql injections.
-  await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
+	// `$1` inserts username into query indirectly, through pg's query-parametisation. this prevents sql injections.
+	await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
+}
+
+async function deleteAllUsernames() {
+	await pool.query("DELETE FROM usernames;");
 }
 
 module.exports = {
-  getAllUsernames,
-  getUsernames,
-  insertUsername,
+	getAllUsernames,
+	getUsernames,
+	insertUsername,
+	deleteAllUsernames,
 };
